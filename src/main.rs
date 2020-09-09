c5cec469ea9f2841a9ab5f371101e7dd2c2340cc
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2020-09-09 16:40:06 -0300

    finalizado

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,8 @@ fn main(){
     println!("The secret number is: {}", secret_number);
     println!("Please input your guess.");
 
+   loop{
+
     let mut guess = String::new();
 
     io::stdin()
@@ -19,13 +21,22 @@ fn main(){
         .expect("Failed to read line");
 
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+    let guess: u32 = match guess.trim().parse(){
+       Ok(num) => num,
+       Err(_) => continue,
+
+    };
 
     println!("You guessed: {}", guess);
+
 
     match guess.cmp(&secret_number) {
         Ordering::Less => println!("Too small!"),
         Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        Ordering::Equal => {
+            println!("You win!");
+            break;
     }
+  }
+   }
 }
