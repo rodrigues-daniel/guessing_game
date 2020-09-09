8f2d300550aab9d58aab995f639815cea10da47a
Daniel Rodrigues <daniel.rodrighes@gmail.com>
2020-09-08 21:27:53 -0300

    number comparation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 use std::io;
+use std::cmp::Ordering;
 use rand::Rng;
+
+
 
 fn main(){
     println!("Guess the number!");
@@ -15,5 +18,14 @@ fn main(){
         .read_line(&mut guess)
         .expect("Failed to read line");
 
+
+    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
